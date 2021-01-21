e6894de3c94ae93e216bc808e7ae9b6aea592b3f
Dyrlien <46710693+Dyrlien@users.noreply.github.com>
2021-01-21 20:16:39 +0100

    converted to Json objects

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -5,25 +5,29 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ConsoleAppTest
 {
     public class Currency
     {
         private string name;
-        private double rate;
-        public Currency(string inputName, double inputRate)
+        private string rate;
+        public Currency(string inputName, string inputRate)
         {
             name = inputName;
             rate = inputRate;
         }
         public string Name
         {
-            get { return name; }            
+            get { return name; }
+            set { name = value; }
         }
-        public double Rate
+        public string Rate
         {
             get { return rate; }
+            set { rate = value; }
         }
         public override string ToString()
         {
@@ -35,7 +39,7 @@ namespace ConsoleAppTest
     {
         public List<Currency> CurrenciesList = new List<Currency>();
                
-        public void AddCurrencies()
+        /*public void AddCurrencies()
         {
             //http://data.fixer.io/api/latest?access_key=a811a7a4f347a1c280eaf781ed121ccb&symbols = GBP, EUR, USD, NOK;
 
@@ -45,7 +49,7 @@ namespace ConsoleAppTest
             CurrenciesList.Add(NOK);
             CurrenciesList.Add(SEK);
             CurrenciesList.Add(USD);
-        }
+        }*/
         public void AvailiableCurrencies()
         {
             Console.WriteLine("Current availiable currencies:");
@@ -122,7 +126,7 @@ namespace ConsoleAppTest
         CurrencyConverter converter = new CurrencyConverter();
         public void Start()
         {
-            test.AddCurrencies();
+            //test.AddCurrencies();
             Console.WriteLine("Welcome to the currency calculator");
             test.AvailiableCurrencies();
             From();
@@ -171,39 +175,99 @@ namespace ConsoleAppTest
     }
     public class WhatIWant
     {
-        public string Base;
-        public string Date;
-        public Rates rates { get; set; }
+        [JsonConverter(typeof(RateConverter))]
+        public List<Currency> Currencies { get; set; }
 
-        public override string ToString()
+        public WhatIWant (List<Currency> testList)
         {
-            
-            return Base +"    "+ Date +"    " + USD;
+            this.Currencies = testList;
         }
+                 
     }
-    public class Rates
+
+    public class RateConverter : JsonConverter
     {
-        public string ID;
-        public double value;
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteStartArray();
+            foreach (var Currency in (List<Currency>)value)
+            {
+                writer.WriteRawValue(JsonConvert.SerializeObject(Currency));
+            }
+            writer.WriteEndArray();
+        }
+
+        // This is when you're reading the JSON object and converting it to C#
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            var response = new List<Currency>();
+            // Loading the JSON object
+            JObject rates = JObject.Load(reader);
+            // Looping through all the properties. C# treats it as key value pair
+            foreach (var rate in rates)
+            {
+                // Finally I'm deserializing the value into an actual Player object
+                var p = JsonConvert.DeserializeObject<Currency>(rate.Value.ToString());
+                // Also using the key as the player Id
+                p.Name = rate.Key;
+                response.Add(p);
+            }
+            Console.WriteLine(response);
+            return response;
+        }
+
+        public override bool CanConvert(Type objectType) => objectType == typeof(List<Currency>);
     }
     public class GetCurrencies
     {
         public async void LoadCurrencies()
+        {
+            HttpClient client = new HttpClient();
+
+            string url = "http://data.fixer.io/api/latest?access_key=a811a7a4f347a1c280eaf781ed121ccb";
+            var response = await client.GetAsync(string.Format(url));
+
+            string result = await response.Content.ReadAsStringAsync();
+
+            JObject data = JObject.Parse(result);
+
+            Console.WriteLine(data["rates"]["USD"]);
+            /*var test = data.Value<JObject>("rates").Properties();
+
+            var test2 = test.ToDictionary(
+                    k => k.Name,
+                    v => v.Value.ToString()
+            );*/
+
+
+        }
+        /*public async void LoadCurrencies()
         {
             string url = "http://data.fixer.io/api/latest?access_key=a811a7a4f347a1c280eaf781ed121ccb";
             using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
             {
                 if (response.IsSuccessStatusCode)
                 {
+                    //string GiveIt = await response.Content.ReadAsStringAsync();
+                    
                     WhatIWant GiveIt = await response.Content.ReadAsAsync<WhatIWant>();
-                    Console.WriteLine(GiveIt.ToString());
+
+                    //System.Threading.Thread.Sleep(20000);
+
+                    string outString = "";
+                    foreach (Currency aCurrency in GiveIt.Currencies)
+                    {
+                        outString += aCurrency.Name + "  :  " + aCurrency.Rate + "\n";
+                    }
+                    Console.WriteLine(outString);
                 }
                 else
                 {
                     Console.WriteLine("did not work");
                 }
             }
-        }
+        }*/
     }
 
     class Program
@@ -214,7 +278,7 @@ namespace ConsoleAppTest
             //Run runprogram = new Run();
             //runprogram.Start();
 
-            ApiHelper.InitializeClient();
+            //ApiHelper.InitializeClient();
             GetCurrencies GetThem = new GetCurrencies();
             GetThem.LoadCurrencies();
             
